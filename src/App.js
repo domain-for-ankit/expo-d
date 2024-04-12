a9145be6063c69414614149c10ed72a39485de5c
amankotharideveloper <amankothari.developer@gmail.com>
2024-04-12 23:28:38 +0530

    merged code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-
+import Sidebar from "./common/Sidebar";
 import './App.css';
 import Header from './component/Header';
 function App() {
   return (
     <div>
       <Header />
+      <Sidebar></Sidebar>
     </div>
   );
 }
